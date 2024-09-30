a7449362fa3411cacc9423a71dc0cc6def7479e8
lenamarochkina <marochkina.lena@gmail.com>
2024-09-30 19:37:51 +0200

    feat: add data cleaning script for ICUSTAYS table

--- Data_cleaning/Data_cleaning_CALLOUT.R
+++ Data_cleaning/Data_cleaning_CALLOUT.R
@@ -3,10 +3,7 @@ callout <- read.csv("data/raw/CALLOUT.csv", stringsAsFactors = TRUE)
 
 # Choose necessary columns
 callout <- callout %>%
-  select(SUBJECT_ID, HADM_ID, CALLOUT_OUTCOME, OUTCOMETIME)
-
-# Read admissions from csv
-admissions <- read.csv("data/raw/cleaned/ADMISSIONS_clean.csv", stringsAsFactors = TRUE)
+  select(ROW_ID, SUBJECT_ID, CALLOUT_OUTCOME, OUTCOMETIME, HADM_ID, REQUEST_TELE, REQUEST_RESP, REQUEST_CDIFF, REQUEST_MRSA, REQUEST_VRE)
 
 # Normalize the columns by converting everything to uppercase
 callout <- callout %>%
@@ -26,9 +23,6 @@ callout_clean <- callout_NA %>%
 # Convert time columns to proper datetime format using lubridate's ymd_hms
 callout_clean <- callout_clean %>%
   mutate(OUTCOMETIME = ymd_hms(OUTCOMETIME)) 
-
-admissions <- admissions %>%
-  mutate(ADMITTIME = as.POSIXct(ADMITTIME, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")) 
 
 # Check if all times have valid hours (0-23), minutes (0-59), and seconds (0-59)
 time_validation <- callout_clean %>%
@@ -62,39 +56,6 @@ if (nrow(inconsistent_rows) > 0) {
 } else {
   print("All rows have consistent CALLOUT_OUTCOME and OUTCOMETIME values.")
 }
-
-# Group by SUBJECT_ID and HADM_ID to find duplicate rows
-admissions_duplicates <- admissions %>%
-  group_by(SUBJECT_ID, HADM_ID) %>%
-  filter(n() > 1)
-
-# Check if there are any duplicates and if ADMITTIME is different for the same SUBJECT_ID and HADM_ID
-admissions_time_check <- admissions_duplicates %>%
-  summarize(admit_times_different = length(unique(ADMITTIME)) > 1)
-
-# Filter rows where ADMITTIME is different
-admissions_time_diff <- admissions_time_check %>%
-  filter(admit_times_different == TRUE)
-
-# Filter admission data to choose distinct rows with certain columns 
-admissions <- admissions %>%
-  select(SUBJECT_ID, HADM_ID, ADMITTIME) %>%
-  distinct()
-
-# This will retain all rows from the callout_data table
-callout_clean <- callout_clean %>%
-  left_join(admissions, 
-            by = c("SUBJECT_ID", "HADM_ID"))
-
-# Convert both ADMITTIME and OUTCOMETIME to numeric (seconds since Unix epoch)
-callout_clean <- callout_clean %>%
-  mutate(TIMEINICU = ifelse(!is.na(ADMITTIME) & !is.na(OUTCOMETIME) & CALLOUT_OUTCOME == 'DISCHARGED',
-                            as.numeric(difftime(OUTCOMETIME, ADMITTIME, units = "secs")),
-                            NA))
-
-# Delete unnecessary ADMITTIME row
-callout_clean <- callout_clean %>%
-  select(-ADMITTIME)
 
 # Write cleaned admissions to csv
 write.csv(callout_clean, "data/raw/cleaned/CALLOUT_clean.csv")
---
+++ Data_cleaning/Data_cleaning_ICUSTAYS.R
@@ -0,0 +1,39 @@
+# Read icu_stays from csv
+icu_stays <- read.csv("data/raw/ICUSTAYS.csv", stringsAsFactors = TRUE)
+
+# Read admissions from csv
+admissions <- read.csv("data/raw/cleaned/ADMISSIONS_clean.csv", stringsAsFactors = TRUE)
+
+# Normalize the columns by converting everything to uppercase
+icu_stays <- icu_stays %>%
+  mutate(across(where(is.factor), function(x) toupper(x) %>% as.factor()))
+
+# Replace empty spaces with NA
+icu_stays_NA <- icu_stays %>%
+  mutate(across(where(is.factor), function(x) as.character(x))) %>%
+  mutate(across(where(is.character), ~ na_if(., ""))) %>%
+  mutate(across(where(is.character), function(x) as.factor(x)))
+
+# Remove exact duplicate rows
+icu_stays_clean <- icu_stays_NA %>%
+  distinct()
+
+# Change datetime format
+# Convert time columns to proper datetime format using lubridate's ymd_hms
+icu_stays_clean <- icu_stays_clean %>%
+  mutate((INTIME = ymd_hms(INTIME)),
+         (OUTTIME = ymd_hms(OUTTIME))) 
+
+# Check if all times have valid hours (0-23), minutes (0-59), and seconds (0-59)
+time_validation <- icu_stays_clean %>%
+  mutate(valid_time = 
+           (is.na(INTIME) | (hour(INTIME) %in% 0:23 & minute(INTIME) %in% 0:59 & second(INTIME) %in% 0:59)),
+           (is.na(OUTTIME) | (hour(OUTTIME) %in% 0:23 & minute(OUTTIME) %in% 0:59 & second(OUTTIME) %in% 0:59)))
+
+# View rows with invalid time
+invalid_time_rows <- time_validation %>%
+  filter(valid_time == FALSE)
+
+# Select only necessary columns
+icu_stays_clean <- icu_stays_clean %>%
+  select(SUBJECT_ID, HADM_ID, ICUSTAY_ID, INTIME, OUTTIME, LOS)
